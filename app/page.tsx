3b722c9c4a3030d913f501635989b1ce3092df1f
BoitshokoSoomo <soomoboitshoko@gmail.com>
2025-05-30 21:05:25 +0200

    Implemented Auth, Billing, Database setup,created companion server action

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,45 @@
 import React from 'react'
 import {Button} from "@/components/ui/button";
+import CompanionCard from "@/components/CompanionCard";
+import CompanionsList from "@/components/CompanionsList";
+import CTA from "@/components/CTA";
 
 const Page = () => {
   return (
-    <div>
-      <h1 className="text-2xl underline">Welcome to my SaaS App</h1>
-        <Button>
-            Lets get started!
-        </Button>
-    </div>
+    <main>
+      <h1>Popular Companions</h1>
+        <section className="home-section">
+            <CompanionCard
+                id="123"
+                name="Neura the brainy explorer"
+                topic="Neural Network of the Brain"
+                subject="science"
+                duration={46}
+                color="#E5D0FF"
+            />
+            <CompanionCard
+                id="456"
+                name="Countsy the number wizard"
+                topic="Derivatives & Integrals"
+                subject="Maths"
+                duration={45}
+                color="#ffda6e"
+            />
+            <CompanionCard
+                id="789"
+                name="Verba the vocabulary builder"
+                topic="language"
+                subject="English Literature"
+                duration={30}
+                color="#BDE7FF"
+            />
+        </section>
+
+        <section className="home-section">
+            <CompanionsList />
+            <CTA />
+        </section>
+    </main>
   )
 }
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,10 @@ export default {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+  	screens: {
+  		'min-lg': '1024px',
+  		'min-2xl': '1536px',
+  	},
   	extend: {
   		colors: {
   			background: 'hsl(var(--background))',
@@ -49,12 +53,15 @@ export default {
   				'3': 'hsl(var(--chart-3))',
   				'4': 'hsl(var(--chart-4))',
   				'5': 'hsl(var(--chart-5))'
-  			}
+  			},
+  			cta: '#000000',
+  			'cta-gold': '#FFD700'
   		},
   		borderRadius: {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
-  			sm: 'calc(var(--radius) - 4px)'
+  			sm: 'calc(var(--radius) - 4px)',
+  			'4xl': '2rem'
   		}
   	}
   },
